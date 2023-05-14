0523839b40b87710edde538d20dee43de379e55a
Gemmus <decadentcrown@gmail.com>
2023-05-14 10:53:12 +0300

    Progression and comments added.

--- sudoku_game.py
+++ sudoku_game.py
@@ -1,24 +1,47 @@
 import pygame
+import requests
+
+########################
+#   Global Variables   #
+########################
 
 width = 800
 play_width = 550
 background_color = (255, 255, 255)
 
-top_left_x = (width - play_width) // 2
-top_left_y = (width - play_width)
+#######################
+#   Calling the API   #
+#######################
+
+response = requests.get("https://sugoku.herokuapp.com/board?difficulty=medium")
+# Available difficulties: easy, medium, hard, random
+grid = response.json()['board']
+grid_original = [[grid[x][y] for y in range(len(grid[0]))] for x in range(len(grid))]
 
 
 def main():
+    ###########################
+    #   Setting up the Game   #
+    ###########################
+
     pygame.init()
     win = pygame.display.set_mode((play_width, play_width))
     pygame.display.set_caption("Sudoku")
     win.fill(background_color)
 
+    #########################
+    #   Drawing the Title   #
+    #########################
+
     pygame.font.init()
     font = pygame.font.SysFont('arial', 40, bold=True)
     game_label = font.render('SUDOKU', 1, (0, 0, 0))
 
-    win.blit(game_label, (width / 3 - game_label.get_width() / 2 + 9, 4))
+    win.blit(game_label, (width / 2 - game_label.get_width() / 2 + 26, 12))
+
+    ##########################
+    #   Creating the Grids   #
+    ##########################
 
     for i in range(10):
         if i % 3 == 0:
@@ -27,6 +50,10 @@ def main():
         pygame.draw.line(win, (0, 0, 0), (50 + 50 * i, 50), (50 + 50 * i, 500), 2)
         pygame.draw.line(win, (0, 0, 0), (50, 50 + 50 * i), (500, 50 + 50 * i), 2)
     pygame.display.update()
+
+    ##########################################
+    #   Placing the Numbers into the Grids   #
+    ##########################################
 
     while True:
         for event in pygame.event.get():
