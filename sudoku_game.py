a45684631c7608078442aa4ac0745586661afa08
Gemmus <decadentcrown@gmail.com>
2023-05-14 10:30:04 +0300

    Sudoku text added.

--- sudoku_game.py
+++ sudoku_game.py
@@ -1,14 +1,24 @@
 import pygame
 
-width = 550
+width = 800
+play_width = 550
 background_color = (255, 255, 255)
+
+top_left_x = (width - play_width) // 2
+top_left_y = (width - play_width)
 
 
 def main():
     pygame.init()
-    win = pygame.display.set_mode((width, width))
+    win = pygame.display.set_mode((play_width, play_width))
     pygame.display.set_caption("Sudoku")
     win.fill(background_color)
+
+    pygame.font.init()
+    font = pygame.font.SysFont('arial', 40, bold=True)
+    game_label = font.render('SUDOKU', 1, (0, 0, 0))
+
+    win.blit(game_label, (width / 3 - game_label.get_width() / 2 + 9, 4))
 
     for i in range(10):
         if i % 3 == 0:
