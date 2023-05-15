3311b7528e81f3ad6eb56cb3478c4c693f146898
Gemmus <decadentcrown@gmail.com>
2023-05-15 08:53:09 +0300

    Progress and functions created.

--- sudoku_game.py
+++ sudoku_game.py
@@ -41,8 +41,49 @@ print(grid)
 #             grid_original[i][j] = 0
 
 grid_original = [[0, 0, 0, 4, 6, 0, 0, 0, 0], [0, 8, 0, 2, 0, 0, 7, 3, 0], [9, 0, 0, 0, 0, 7, 0, 0, 0], [6, 0, 0, 0, 0, 2, 0, 4, 0], [0, 1, 5, 0, 0, 0, 2, 0, 9], [0, 4, 0, 0, 0, 8, 0, 0, 0], [0, 0, 0, 0, 0, 6, 0, 0, 1], [7, 0, 0, 0, 0, 4, 9, 0, 3], [0, 0, 9, 0, 0, 0, 5, 0, 0]]
-
 print(grid_original)
+
+
+######################################
+#   Function for drawing the title   #
+######################################
+
+def draw_title(win):
+    pygame.font.init()
+    title_font = pygame.font.SysFont('arial', 40, bold=False)
+    game_label = title_font.render('SUDOKU', 1, (0, 0, 0))
+
+    win.blit(game_label, (width / 2 - game_label.get_width() / 2 + 26, 12))
+
+
+#######################################
+#   Function for creating the grids   #
+#######################################
+
+def draw_grid(win):
+    for i in range(10):
+        if i % 3 == 0:
+            pygame.draw.line(win, (0, 0, 0), (50 + 50 * i, 50), (50 + 50 * i, 500), 4)
+            pygame.draw.line(win, (0, 0, 0), (50, 50 + 50 * i), (500, 50 + 50 * i), 4)
+        pygame.draw.line(win, (0, 0, 0), (50 + 50 * i, 50), (50 + 50 * i, 500), 2)
+        pygame.draw.line(win, (0, 0, 0), (50, 50 + 50 * i), (500, 50 + 50 * i), 2)
+    pygame.display.update()
+
+
+#######################################################
+#   Function for placing the Numbers into the Grids   #
+#######################################################
+
+def place_numbers(win):
+    font = pygame.font.SysFont('arial', 35)
+
+    for i in range(9):
+        for ii in range(9):
+            if 0 < grid_original[i][ii] < 10:
+                value = font.render(str(grid[9*i+ii]), 1, (0, 0, 0))
+                win.blit(value, ((ii + 1) * 50 + 18, (i + 1) * 50 + 8))
+
+    pygame.display.update()
 
 
 ################################
@@ -65,31 +106,47 @@ def insert(win, position):
                         return
                     else:
                         if event.key == pygame.K_1 or event.key == pygame.K_KP1:
+                            pygame.draw.rect(win, (25, 143, 251), (x * 54, y * 54, 45, 45))
+                            value = font.render(str(1), 1, (255, 102, 15))
+                            win.blit(value, (x * 54, y * 54))
+                            pygame.display.update()
                             print('1')
                             return
                         if event.key == pygame.K_2 or event.key == pygame.K_KP2:
+                            pygame.draw.rect(win, (25, 143, 251), (x * 54, y * 54, 45, 45))
                             print('2')
                             return
                         if event.key == pygame.K_3 or event.key == pygame.K_KP3:
+                            pygame.draw.rect(win, (25, 143, 251), (x * 54, y * 54, 45, 45))
                             print('3')
                             return
                         if event.key == pygame.K_4 or event.key == pygame.K_KP4:
+                            pygame.draw.rect(win, (25, 143, 251), (x * 54, y * 54, 45, 45))
                             print('4')
                             return
                         if event.key == pygame.K_5 or event.key == pygame.K_KP5:
+                            pygame.draw.rect(win, (25, 143, 251), (x * 54, y * 54, 45, 45))
                             print('5')
                             return
                         if event.key == pygame.K_6 or event.key == pygame.K_KP6:
+                            pygame.draw.rect(win, (25, 143, 251), (x * 54, y * 54, 45, 45))
                             print('6')
                             return
                         if event.key == pygame.K_7 or event.key == pygame.K_KP7:
+                            pygame.draw.rect(win, (25, 143, 251), (x * 54, y * 54, 45, 45))
                             print('7')
                             return
                         if event.key == pygame.K_8 or event.key == pygame.K_KP8:
+                            pygame.draw.rect(win, (25, 143, 251), (x * 54, y * 54, 45, 45))
                             print('8')
                             return
                         if event.key == pygame.K_9 or event.key == pygame.K_KP9:
+                            pygame.draw.rect(win, (25, 143, 251), (x * 54, y * 54, 45, 45))
                             print('9')
+                            return
+                        if event.key == pygame.K_0 or event.key == pygame.K_KP_0:
+                            pygame.draw.rect(win, (0, 0, 0), (x * 54, y * 54, 45, 45))
+                            pygame.display.update()
                             return
 
 
@@ -128,37 +185,19 @@ def main():
     #   Drawing the Title   #
     #########################
 
-    pygame.font.init()
-    title_font = pygame.font.SysFont('arial', 40, bold=False)
-    game_label = title_font.render('SUDOKU', 1, (0, 0, 0))
-
-    win.blit(game_label, (width / 2 - game_label.get_width() / 2 + 26, 12))
+    draw_title(win)
 
     ##########################
     #   Creating the Grids   #
     ##########################
 
-    for i in range(10):
-        if i % 3 == 0:
-            pygame.draw.line(win, (0, 0, 0), (50 + 50 * i, 50), (50 + 50 * i, 500), 4)
-            pygame.draw.line(win, (0, 0, 0), (50, 50 + 50 * i), (500, 50 + 50 * i), 4)
-        pygame.draw.line(win, (0, 0, 0), (50 + 50 * i, 50), (50 + 50 * i, 500), 2)
-        pygame.draw.line(win, (0, 0, 0), (50, 50 + 50 * i), (500, 50 + 50 * i), 2)
-    pygame.display.update()
+    draw_grid(win)
 
     ##########################################
     #   Placing the Numbers into the Grids   #
     ##########################################
 
-    font = pygame.font.SysFont('arial', 35)
-
-    for i in range(9):
-        for ii in range(9):
-            if 0 < grid_original[i][ii] < 10:
-                value = font.render(str(grid[9*i+ii]), 1, (0, 0, 0))
-                win.blit(value, ((ii + 1) * 50 + 18, (i + 1) * 50 + 8))
-
-    pygame.display.update()
+    place_numbers(win)
 
     ########################
     #   Quiting the game   #
